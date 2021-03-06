6e09aeb81f902878c230644d54ef624bdb6e417a
lchia <lichenghua2014@ia.ac.cn>
2021-03-06 22:08:07 +0800

    add DF2K making scripts

---
+++ code/run_preparedata.sh
@@ -0,0 +1,19 @@
+# prepare_data.py \
+	# HR image path
+	# patch_size
+	# save_dir
+	# save_tag
+
+# Flikr2K
+# python prepare_data.py \
+# 	../../../data/Flickr2K/Flickr2K_HR/ \
+# 	512 \
+# 	../../../data/Flickr2K/Flickr2K_pkls/ \
+# 	Flickr2K \
+
+# DF2K
+python prepare_data.py \
+	../../../data/DF2K/ \
+	512 \
+	../../../data/DF2K_pkls/ \
+	DF2K \
